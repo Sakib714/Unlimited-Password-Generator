624b9e806ba07a8db8aa69363ae3ca924123d5a7
Sadman Sakib <78845308+Sakib714@users.noreply.github.com>
2021-08-27 19:02:00 +0000

    Done Making Non-Coder Friendly!😀

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import random
 import string
-
+from time import sleep
 
 length = int(input('Password Length: '))
 counter = 1
 while True:
+    if counter == 20000:
+        print('Enter Y To Continue Infinite Times(This May Hang Your Device  If You Have Low Ram And Continue Running This Programme More then 10 Minutes!\nEnter N To Stop The Programme!')
+        ui = input('Your Reply: ')
+        if ui =='Y':
+            break
+        elif ui == 'N':
+            pass
+        else:
+            print('Wrong Input!')
+            sleep(1)
     password = ''.join(random.choice(string.ascii_letters + string.punctuation) for i in range(length))
     print('Password No',counter,'is :',  password)
     counter+=1
